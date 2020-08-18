2cdccd74bc97e1860b049b1c71fda6bacfc835bf
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-18 20:33:11 +0000

    Add BootcampX Queries 4

---
+++ 4-queries/10-_most_confusing_assignment.sql
@@ -0,0 +1,7 @@
+ 
+SELECT assignments.id, assignments.day, assignments.chapter, assignments.name, COUNT(assistance_requests.*) AS total_requests 
+  FROM assistance_requests
+  JOIN assignments ON assignments.id=assistance_requests.assignment_id
+  GROUP BY assignments.id
+  ORDER BY total_requests DESC
+  ;
